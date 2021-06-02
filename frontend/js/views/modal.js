31a331a6710abf305a2912662e98d81b559c227d
Julian Kniephoff <kniephoff@elan-ev.de>
2021-06-02 09:02:19 +0200

    Add a basic modal base view

---
+++ frontend/js/views/modal.js
@@ -0,0 +1,74 @@
+/**
+ *  Copyright 2021, ELAN e.V., Germany
+ *  Licensed under the Educational Community License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance
+ *  with the License. You may obtain a copy of the License at
+ *
+ *  http://www.osedu.org/licenses/ECL-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an "AS IS"
+ *  BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ *  or implied. See the License for the specific language governing
+ *  permissions and limitations under the License.
+ */
+
+define([
+    "underscore",
+    "backbone",
+    "bootstrap"
+], function (
+    _,
+    Backbone
+) {
+    "use strict";
+
+    var Modal = Backbone.View.extend({
+
+        className: function () {
+            return (this.additionalClassName || "") + " modal";
+        },
+
+        attributes: function () {
+            return _.extend({ tabindex: -1 }, this.additionalAttributes);
+        },
+
+        show: function (options) {
+            this.$el.modal(
+                _.chain(this.modalOptions)
+                    .extend(options)
+                    .extend({ show: true })
+                    .value()
+            );
+        },
+
+        hide: function () {
+            this.$el.modal("hide");
+        },
+
+        initialize: function (options) {
+            _.chain(this.modalOptions)
+                .defaults({
+                    // By default, the modal should not be closable implicitly,
+                    // so that the client is forced to take care of any cleanup logic.
+                    // Additionally, we are truly modal by default,
+                    // i.e. the user can't muck about with the state of the app
+                    // outside of the dialog.
+                    backdrop: "static",
+                    keyboard: false
+                })
+                .extend(options);
+
+            if (this.hidden) {
+                this.$el.on("hidden", _.bind(this.hidden, this));
+            }
+        },
+
+        remove: function () {
+            this.$el.on("hidden", _.bind(Backbone.View.prototype.remove, this));
+            this.hide();
+        }
+    });
+
+    return Modal;
+});
